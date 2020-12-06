115aaebc9167f1fe127c6c45088e4f1dae70a12b
luanvo1ad <luanvo1ad@gmail.com>
2020-12-06 07:53:12 +0700

    Add project files.

---
+++ Scripts/script.js
@@ -0,0 +1,362 @@
+﻿$(function () {
+    $("#main-carousel.owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        responsive: {
+            0: {
+                nav: false
+            },
+            576: {
+                nav: false
+            },
+            768: {
+            },
+            992: {
+            },
+            1200: {
+            }
+        },
+        loop: true,
+        margin: 25,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        dots: true,
+    });
+    $('#main-carousel.owl-carousel').on('translate.owl.carousel', function (e) {
+        var obj = {
+            animateOut: ['animate__fadeOut', 'animate__FadeOutDown', 'animate__FadeOutLeft', 'animate__FadeOutRight', 'animate__FadeOutUp', 'animate__FadeOutTopLeft', 'animate__FadeOutTopRight', 'animate__FadeOutBottomLeft', 'animate__FadeOutBottomRight'],
+            animateIn: ['animate__fadeIn', 'animate__fadeInDown', 'animate__fadeInUp', 'animate__flipInX', 'animate__flipInY', 'animate__rotateIn', 'animate__jackInTheBox', 'animate__zoomIn', 'animate__zoomInDown', 'animate__zoomInUp', 'animate__slideInDown', 'animate__slideInUp', 'animate__backInDown', 'animate__backInUp', 'animate__bounceIn', 'animate__bounceInDown', 'animate__bounceInUp']
+        }
+        var numbTem = Math.floor(Math.random() * obj.animateIn.length) //rounding number
+
+
+        var index = e.item.index;
+        for (var i = 0; i < obj.animateIn.length; i++) {
+            $('.owl-item').removeClass(obj.animateIn[i]);
+
+        }
+        $('.owl-item').removeClass('animate__animated');
+        $('.owl-item').eq(index).addClass('animate__animated ' + obj.animateIn[numbTem]);
+    });
+    $("#main-category.owl-carousel").owlCarousel({
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 3,
+            },
+            576: {
+                items: 4,
+            },
+            768: {
+                items: 5,
+            },
+            992: {
+                items: 6,
+            },
+            1200: {
+                items: 8,
+            }
+        },
+        loop: true,
+        margin: 10,
+        autoplay: false,
+        nav: true,
+        dots: true,
+    });
+    $("#web-rating.owl-carousel").owlCarousel({
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                items: 3,
+            },
+            1200: {
+                items: 4,
+            }
+        },
+        dots: true,
+        loop: true,
+        margin: 15,
+        autoplay: false,
+    });
+    $(".owl-list.owl-carousel").owlCarousel({
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            576: {
+                items: 2,
+            },
+            768: {
+                items: 3,
+            },
+            992: {
+                items: 3,
+            },
+            1200: {
+                items: 4,
+            }
+        },
+        loop: true,
+        margin: 15,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        dots: true,
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+            $('#back-to-top').removeClass('active');
+        }
+    });
+    $('#back-to-top').click(function () {
+        $('#back-to-top').addClass('active');
+        $('#back-to-top .fa').addClass('animate__fadeOutUpBig animate__animated animate__slower');
+        setTimeout(() => {
+            $('#back-to-top .fa').removeClass('animate__fadeOutUpBig animate__animated animate__slower')
+        }, 2000);
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+
+    $(".news .item").on("mouseover", function () {
+        $(this).find(".item-title").addClass("animate__animated animate__bounceOut");
+    });
+    $(".news .item").on("mouseleave", function () {
+        $(this).find(".item-title").removeClass("animate__animated animate__bounceOut");
+    })
+
+    $(".product .item").on("mouseover", function () {
+        $(this).addClass("animate__animated animate__swing animate__fast");
+    });
+    $(".product .item").on("mouseleave", function () {
+        $(this).removeClass("animate__animated animate__swing animate__fast");
+    })
+
+    $('.money').mask('000.000.000.000.000đ', { reverse: true });
+    $('.phone-number').mask("000-000-0000", { selectOnFocus: true });
+
+    $(".item").on("mouseover", function () {
+        $(this).addClass("shadow-dreamy");
+    });
+    $(".item").on("mouseleave", function () {
+        $(this).removeClass("shadow-dreamy");
+    })
+
+    //facncy
+    $.fn.fancyMorph = function (opts) {
+
+        var Morphing = function ($btn, opts) {
+            var self = this;
+
+            self.opts = $.extend({
+                animationEffect: false,
+                infobar: false,
+                buttons: ['close'],
+                smallBtn: false,
+                touch: false,
+                baseClass: 'fancybox-morphing',
+                afterClose: function () {
+                    self.close();
+                }
+            }, opts);
+
+            self.init($btn);
+        };
+
+        Morphing.prototype.init = function ($btn) {
+            var self = this;
+
+            self.$btn = $btn.addClass('morphing-btn');
+
+            self.$clone = $('<div class="morphing-btn-clone" />')
+                .hide()
+                .insertAfter($btn);
+
+            // Add wrapping element and set initial width used for positioning
+            $btn.wrap('<span class="morphing-btn-wrap"></span>').on('click', function (e) {
+                e.preventDefault();
+
+                self.start();
+            });
+
+        };
+
+        Morphing.prototype.start = function () {
+            var self = this;
+
+            if (self.$btn.hasClass('morphing-btn_circle')) {
+                return;
+            }
+
+            // Set initial width, because it is not possible to start CSS transition from "auto"
+            self.$btn.width(self.$btn.width()).parent().width(self.$btn.outerWidth());
+
+            self.$btn.off('.fm').on("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function (e) {
+
+                if (e.originalEvent.propertyName === 'width') {
+                    self.$btn.off('.fm');
+
+                    self.animateBg();
+                }
+
+            }).addClass('morphing-btn_circle');
+
+        };
+
+        Morphing.prototype.animateBg = function () {
+            var self = this;
+
+            self.scaleBg();
+
+            self.$clone.show();
+
+            // Trigger repaint
+            self.$clone[0].offsetHeight;
+
+            self.$clone.off('.fm').on("transitionend.fm webkitTransitionEnd.fm oTransitionEnd.fm MSTransitionEnd.fm", function (e) {
+                self.$clone.off('.fm');
+
+                self.complete();
+
+            }).addClass('morphing-btn-clone_visible');
+        };
+
+        Morphing.prototype.scaleBg = function () {
+            var self = this;
+
+            var $clone = self.$clone;
+            var scale = self.getScale();
+            var $btn = self.$btn;
+            var pos = $btn.offset();
+
+            $clone.css({
+                top: pos.top + $btn.outerHeight() * 0.5 - ($btn.outerHeight() * scale * 0.5) - $(window).scrollTop(),
+                left: pos.left + $btn.outerWidth() * 0.5 - ($btn.outerWidth() * scale * 0.5) - $(window).scrollLeft(),
+                width: $btn.outerWidth() * scale,
+                height: $btn.outerHeight() * scale,
+                transform: 'scale(' + (1 / scale) + ')'
+            });
+        };
+
+        Morphing.prototype.getScale = function () {
+            var $btn = this.$btn,
+                radius = $btn.outerWidth() * 0.5,
+                left = $btn.offset().left + radius - $(window).scrollLeft(),
+                top = $btn.offset().top + radius - $(window).scrollTop(),
+                windowW = $(window).width(),
+                windowH = $(window).height();
+
+            var maxDistHor = (left > windowW / 2) ? left : (windowW - left),
+                maxDistVert = (top > windowH / 2) ? top : (windowH - top);
+
+            return Math.ceil(Math.sqrt(Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2)) / radius);
+        };
+
+        Morphing.prototype.complete = function () {
+            var self = this;
+            var $btn = self.$btn;
+
+            $.fancybox.open({ src: $btn.data('src') || $btn.attr('href') }, self.opts);
+
+            $(window).on('resize.fm', function () {
+                //self.scaleBg();
+            });
+        };
+
+        Morphing.prototype.close = function () {
+            var self = this;
+            var $clone = self.$clone;
+
+            self.scaleBg();
+
+            $clone.one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function (e) {
+                $clone.hide();
+
+                self.$btn.removeClass('morphing-btn_circle');
+            });
+
+            $clone.removeClass('morphing-btn-clone_visible');
+
+            $(window).off('resize.fm');
+        };
+
+        // Init
+        this.each(function () {
+            var $this = $(this);
+
+            if (!$this.data("morphing")) {
+                $this.data("morphing", new Morphing($this, opts));
+            }
+
+        });
+
+        return this;
+    };
+
+    $("[data-morphing]").fancyMorph({
+        hash: 'morphing'
+    });
+
+    $(".detail").find("img").addClass("d-block w-100");
+
+    $('[data-toggle="tooltip"]').tooltip({
+        delay: { "show": 800, "hide": 300 }
+    })
+
+    $(".navbar-toggler").click(function () {
+        $(this).find(".fa").toggleClass("on");
+    });
+
+    $(".dropdown-menu").addClass("animate-dropdown slideIn");
+
+    AOS.init({
+        easing: 'ease-out-back',
+        duration: 1000
+    });
+
+    $('.intro .icon').on({
+        mouseover: function () {
+            $(this).addClass('animate__flip animate__animated');
+            setTimeout(() => {
+                $(this).removeClass('animate__flip animate__animated')
+            }, 2000);
+        },
+        click: function () {
+            $(this).addClass('animate__flip animate__animated');
+            setTimeout(() => {
+                $(this).removeClass('animate__flip animate__animated')
+            }, 2000);
+        },
+    });
+    $(window).on('load',function () {
+        $('.intro .icon').addClass('animate__flip animate__animated');
+        setTimeout(() => {
+            $('.intro .icon').removeClass('animate__flip animate__animated')
+        }, 2000);
+    })
+
+    //luoi`....
+    $('.product .item .btn.btn-danger').addClass('btn-buy');
+
+    //loader
+    setTimeout(() => {
+        $('#loading').hide();
+    }, 1000);
+});
+
---
+++ admin/Scripts/script.js
@@ -0,0 +1,139 @@
+﻿function bs_input_file() {
+    $(".btn-upload-img").click(function () {
+        $('button.btn-choose').click();
+    });
+
+    $(".upload-container .close").click(function () {
+        $('.upload-container img').attr('src', 'images/icons/no-img.png');
+        $('.input-ghost').val('');
+    });
+
+    $(".input-file").before(
+        function () {
+            if (!$(this).prev().hasClass('input-ghost')) {
+                var element = $("<input type='file' class='input-ghost' style='visibility:hidden; height:0'>");
+                element.attr("name", $(this).attr("name"));
+                element.change(function () {
+                    element.next(element).find('input').val((element.val()).split('\\').pop());
+                });
+                $(this).find("button.btn-choose").click(function () {
+                    element.click();
+                });
+                $(this).find("button.btn-reset").click(function () {
+                    element.val(null);
+                    $(this).parents(".input-file").find('input').val('');
+                });
+                $(this).find('input').css("cursor", "pointer");
+                $(this).find('input').mousedown(function () {
+                    $(this).parents('.input-file').prev().click();
+                    return false;
+                });
+                return element;
+            }
+        }
+    );
+
+    $(".input-ghost").change(function () {
+        readURL(this);
+    });
+
+}
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('.upload-container img').attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+function autoYoutube() {
+
+    $(".youtube-url").on("input", function () {
+        var value = $(this).val();
+        var id = youtubeUrlToID(value);
+
+        var iframe = $("#youtube-embed iframe");
+        iframe.attr("src", "https://www.youtube.com/embed/" + id)
+
+        var img = $(".youtube-thumb");
+        img.attr("src", "https://img.youtube.com/vi/#/0.jpg".replace("#",id));
+    })
+
+}
+
+function youtubeUrlToID(url) {
+    var newval = '';
+
+    if (newval = url.match(/(\?|&)v=([^&#]+)/)) {
+
+        return newval.pop();
+
+    } else if (newval = url.match(/(\.be\/)+([^\/]+)/)) {
+
+        return newval.pop();
+
+    } else if (newval = url.match(/(\embed\/)+([^\/]+)/)) {
+
+        return newval.pop().replace('?rel=0', '');
+
+    }
+
+}
+function createButtonForPictureList() {
+    //tìm tất cả thẻ .card hiện có
+    $("#picture-list .card").append(" <a href='#' class='btn btn-success btn-sm'><i class='fa fa-picture-o'></i>Thay hình</a>");
+
+    // lọc theo từng card thêm vào 1 đoạn html nút nhấn
+}
+
+$(function () {
+    $("#toogle-button").click(function () {
+        //Kiểm tra trạng thái nút bấm
+        if ($(this).hasClass("toogle-button close")) {
+            $(this).removeClass("close");
+            $("#main-menu").removeClass("col-lg-1").addClass("col-lg-4");
+            $("#main-content").removeClass("col-lg-11").addClass("col-lg-8");
+            $("#main-menu .menu .collapse .card-body").show();
+            $("#main-menu .menu .card-header button:nth-child(2)").show();
+            $("#main-menu .menu .card-header button:nth-child(1) i").removeClass("fa-3x");
+            $("#main-menu .menu .card-header button:nth-child(1)").removeClass("fa-10px");
+            $("#main-menu .account-info div").show();
+            $("#main-menu .welcome-box span").show();
+            $("#main-menu .welcome-box i").removeClass("center-align");
+            $("#main-menu .footer .btn").show();
+        }
+        else {
+            $(this).addClass("close");
+            $("#main-menu").removeClass("col-lg-4").addClass("col-lg-1");
+            $("#main-content").removeClass("col-lg-8").addClass("col-lg-11");
+            $("#main-menu .menu .collapse .card-body").hide();
+            $("#main-menu .menu .card-header button:nth-child(2)").hide();
+            $("#main-menu .menu .card-header button:nth-child(1) i").addClass("fa-3x");
+            $("#main-menu .menu .card-header button:nth-child(1)").addClass("fa-10px");
+            $("#main-menu .account-info div").hide();
+            $("#main-menu .welcome-box span").hide();
+            $("#main-menu .welcome-box i").addClass("center-align");
+            $("#main-menu .footer .btn").hide();
+            $("#main-menu .footer .btn:last-child").show();
+        }
+    });
+    //input_ArticelEdit
+    bs_input_file();
+    autoYoutube();
+    createButtonForPictureList();
+    $(".btnEdit").click(function () {
+        $(this).parents("tr").addClass("d-none");
+        $(this).parents("tr").next().removeClass("d-none");
+        $(this).parents("tr").next().find(".btnCancel").addClass("show")
+    });
+
+    $(".btnCancel.show").click(function () {
+        $(this).parents("tr").addClass("d-none");
+        $(this).parents("tr").prev().removeClass("d-none");
+        $(this).removeClass("show");
+    });
+});
